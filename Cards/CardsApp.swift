6023a5957d5bde0053cfe3b682c1f7b04cdd6c13
bestja210 <121860531+bestja210@users.noreply.github.com>
2023-05-10 14:29:44 -0400

    Create CardsApp.swift

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Jacob Best on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
