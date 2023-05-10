97c4e3d48c582ccb061dd6d77c77f54984720a5d
bestja210 <121860531+bestja210@users.noreply.github.com>
2023-05-10 14:31:31 -0400

    Add files via upload

---
+++ Cards/Models/CustomColors.swift
@@ -0,0 +1,16 @@
+//
+//  CustomColors.swift
+//  Cards
+//
+//  Created by Jacob Best on 2/14/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomColors {
+    static let backgroundColor = Color("backgroundColor")
+    static let cardColor = Color("cardColor")
+}
+
+
